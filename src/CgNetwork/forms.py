5ba2a1a75ef90b09c7c90cfa2c58d80fdf604812
wam <wam@pop-os.localdomain>
2023-04-05 10:08:41 +0200

    clean

--- src/CgNetwork/forms.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm
-from CgNetwork.models import CustomUser
-
-
-class UserRegistrationForm(UserCreationForm):
-    class Meta:
-        model = CustomUser
-        fields = ['user_role', 'email']
